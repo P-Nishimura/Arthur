0c13dcf809a80ee9c037bd44e03f8d8c00ccdb5b
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2018-12-31 06:27:36 +0000

    ユーザーテーブルに名前、動機、生年月日のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_23_153926) do
+ActiveRecord::Schema.define(version: 2018_12_31_062329) do
 
   create_table "opinions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2018_12_23_153926) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.text "reason"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
