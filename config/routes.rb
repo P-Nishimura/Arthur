5688021954a46cf2c68e2b21b60bfbabfe8e3d13
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2019-01-01 11:17:14 +0000

    opinionをtopicにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root "topic#index"
-  resources :topic
-  resources :opinion
+  resources :topic do
+    resources :opinion
+  end  
   resources :user
   
 end
