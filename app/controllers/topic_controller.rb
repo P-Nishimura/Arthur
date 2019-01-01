3052001a150dc474e16ddee712aad10c6950b1c7
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2019-01-01 06:15:58 +0000

    Topic新規作成用コントローラ記述

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -1,7 +1,22 @@
 class TopicController < ApplicationController
     
+    before_action :move_to_index, except: [:index]
+    
     def index
+        @topics = Topics.all.order("created_at DESC")
+    end
+    
+    def new
         
+    end
+    
+    def create
+        
+    end
+    
+    private
+    def move_to_index
+        redirect_to action: :index unless user_sign_in?
     end
     
 end
