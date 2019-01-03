f9a0b5d0be51269d5db9b3ee539c1155f8e3d435
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2019-01-03 14:10:46 +0000

    コメント機能追加で、トークルームはひとまず完成。

--- app/controllers/opinion_controller.rb
+++ app/controllers/opinion_controller.rb
@@ -1,2 +1,12 @@
 class OpinionController < ApplicationController
+    
+    def create
+        @opinion = Opinion.create(content: opinion_params[:content], user_id: current_user.id, topic_id: opinion_params[:topic_id])
+        redirect_to "/topic/#{@opinion.topic.id}"
+    end
+    
+    private
+    def opinion_params
+        params.permit(:content, :topic_id)
+    end
 end
