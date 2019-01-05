789ae4a17f74f14342abb7f171e203c69c84a679
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2019-01-05 08:56:01 +0000

    opinionの削除機能追加

--- app/controllers/opinion_controller.rb
+++ app/controllers/opinion_controller.rb
@@ -5,6 +5,12 @@ class OpinionController < ApplicationController
         redirect_to "/topic/#{@opinion.topic.id}"
     end
     
+    def destroy
+        opinion = Opinion.find(params[:id])
+        opinion.destroy if current_user.id==opinion.user.id
+        redirect_to "/topic/#{opinion.topic_id}"
+    end
+    
     private
     def opinion_params
         params.permit(:content, :topic_id)
