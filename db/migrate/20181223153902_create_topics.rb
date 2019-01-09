ce9e7acdfb12bce9f8c70b8a56eac749b4173749
EC2 Default User <ec2-user@ip-172-31-39-96.ap-southeast-1.compute.internal>
2019-01-09 08:47:33 +0000

    マイグレーションファイル更新

--- db/migrate/20181223153902_create_topics.rb
+++ db/migrate/20181223153902_create_topics.rb
@@ -4,7 +4,7 @@ class CreateTopics < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :detail
       t.integer :user_id
-      t.boolean :open_or_close?
+      
 
       t.timestamps
     end
